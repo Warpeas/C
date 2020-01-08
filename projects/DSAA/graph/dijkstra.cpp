f75329596cc36b1a36a2648e5a16fd0d6e744cda
Warpeas <M4A1go@163.com>
2020-01-08 17:46:13 +0800

    trying to build Dijkstra

--- projects/DSAA/graph/dijkstra.cpp
+++ projects/DSAA/graph/dijkstra.cpp
@@ -5,7 +5,7 @@
 #include <cstdio>
 
 // Number of vertices in the graph
-#define V 9
+#define V 4
 
 // A utility function to find the vertex with minimum distance value, from
 // the set of vertices not yet included in shortest path tree
@@ -23,8 +23,10 @@ int minDistance(int dist[], bool sptSet[]) {
 // A utility function to print the constructed distance array
 int printSolution(int dist[]) {
   printf("Vertex \t\t Distance from Source\n");
-  for (int i = 0; i < V; i++)
+  for (int i = 0; i < V; i++) {
     printf("%d \t\t %d\n", i, dist[i]);
+  }
+  return 0;
 }
 
 // Function that implements Dijkstra's single source shortest path algorithm
@@ -70,13 +72,8 @@ void dijkstra(int graph[V][V], int src) {
 int main() {
   /* Let us create the example graph discussed above */
   int graph[V][V] = {
-      {0, 4, 0, 0, 0, 0, 0, 8, 0},  {4, 0, 8, 0, 0, 0, 0, 11, 0},
-      {0, 8, 0, 7, 0, 4, 0, 0, 2},  {0, 0, 7, 0, 9, 14, 0, 0, 0},
-      {0, 0, 0, 9, 0, 10, 0, 0, 0}, {0, 0, 4, 14, 10, 0, 2, 0, 0},
-      {0, 0, 0, 0, 0, 2, 0, 1, 6},  {8, 11, 0, 0, 0, 0, 1, 0, 7},
-      {0, 0, 2, 0, 0, 0, 6, 7, 0}};
-
+      {0, 1, 3, 6},  {1, 0, -4, -4},
+      {3, -4, 0, 1},  {6, -4, 1, 0}};
   dijkstra(graph, 0);
-
   return 0;
 }
