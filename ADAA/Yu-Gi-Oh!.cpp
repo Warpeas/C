0df2edc555569809c6f8e52a10a43c8f47853a23
Warpeas <M4A1go@163.com>
2020-03-31 00:08:42 +0800

    some changes

--- ADAA/Yu-Gi-Oh!.cpp
+++ ADAA/Yu-Gi-Oh!.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <hash_map>
 
+using namespace std;
 /*
 |   score   |   points  |
 |   2:0     |   3       |
@@ -8,3 +10,29 @@
 |   1:2     |   1       |
 |   0:2     |   0       |
 */
+
+/*
+hashmap<(string)number+score:(int)cnt>
+*/
+
+/*
+a (n)*(n) matrix
+and a n array store each score
+first dfs, fill each grid
+when the left empty grid is the number n then find if the situation is calculate
+use sorted array? relate with an int
+*/
+int n;
+int dfs(int player[10],int scores[10][10]){
+    __gnu_cxx::hash_map<int[10], int> hm;
+    hm.insert(player[10], 1);
+    cout << hm.begin()->second;
+    return 0;
+}
+
+int main(){
+    int player[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int scores[10][10];
+    dfs(player, scores);
+    return 0;
+}
