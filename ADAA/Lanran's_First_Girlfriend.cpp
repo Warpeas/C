f860ebe27e2fd99a75a885fd4486a7f55fbfb275
Warpeas <M4A1go@163.com>
2020-03-31 12:32:37 +0800

    one clear, to two

--- ADAA/Lanran's_First_Girlfriend.cpp
+++ ADAA/Lanran's_First_Girlfriend.cpp
@@ -1,7 +1,6 @@
 #include <algorithm>
 #include <iostream>
 
-
 using namespace std;
 class trick {
 private:
@@ -9,14 +8,14 @@ private:
   int end;
 
 public:
-  trick();
+  trick(){};
   trick(int s, int e) {
     start = s;
     end = e;
   }
   int getStart() { return start; }
   int getEnd() { return end; }
-  friend bool operator<(trick &t1, trick &t2) { return t1.end < t2.end; }
+  friend bool operator<(const trick &t1, const trick &t2) { return t1.end < t2.end; }
 };
 
 int t, n;
@@ -30,19 +29,19 @@ int main() {
   while (t > 0) {
     --t;
     cin >> n;
-    trick *tricks[n];
+    trick *tricks = new trick[n]();
     int start, end;
     for (int i = 0; i < n; i++) {
       cin >> start >> end;
-      tricks[i] = new trick(start, end);
+      tricks[i] = *new trick(start, end);
     }
     sort(tricks, tricks + n);
     int currentEnd = 0, cnt = 0;
-    for(int i = 0;i < n ;i++){
-    if(tricks[i]->getStart()>currentEnd){
+    for (int i = 0; i < n; i++) {
+      if (tricks[i].getStart() > currentEnd) {
         ++cnt;
-        currentEnd = tricks[i]->getEnd();
-    }
+        currentEnd = tricks[i].getEnd();
+      }
     }
     cout << cnt << "\n";
   }
