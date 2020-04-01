b4b1bd62277cd9ed0de99185863e786d09a1886d
Warpeas <M4A1go@163.com>
2020-04-01 10:06:44 +0800

    thinking

--- ADAA/Lanran's_Second_Girlfriend.cpp
+++ ADAA/Lanran's_Second_Girlfriend.cpp
@@ -29,19 +29,18 @@ int main() {
   cout.tie(0);
   cin >> n;
   trick *tricks = new trick[n]();
-  int start, end, maxInterval = 0, End = 0;
+  int start, end, maxInterval = 0, End = 0, Start = 0;
   for (int i = 0; i < n; i++) {
     cin >> start >> end;
     if (end - start > maxInterval - 1) {
       maxInterval = end - start + 1;
     }
-    if (end > End) {
-      End = end;
-    }
+    End = max(end, End);
+    Start = min(Start, start);
     tricks[i] = *new trick(start, end);
   }
-  maxTime = min(maxInterval, End / n);
-  int high = maxTime / 2, low = 0, result = 0;
+  maxTime = min(maxInterval, (End - Start + 1) / n);
+  int high = maxTime / 2, low = 1, result = 0;
   while (high < low) {
     result = (high + low) / 2;
     if (isValidTime(result)) {
