09eb17b66625fd18845cebdfb16902eaa194ce83
Warpeas <M4A1go@163.com>
2020-04-06 18:12:27 +0800

    priority queue + hashmap version

--- src/FindingVince.java
+++ src/FindingVince.java
@@ -1,11 +1,14 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class FindingVince {
     static int n;
     static vertex[] graph;
-    static long[] dist;
+    //    static long[] dist;
+    static Map<Integer, Long> dist;
+    static PriorityQueue<Map.Entry<Integer, Long>> queue = new PriorityQueue<>(
+            (o1, o2) -> (int) (Math.sqrt(o1.getValue() - o2.getValue()))
+    );
     static InputStream inputStream = System.in;
     static OutputStream outputStream = System.out;
     static InputReader in = new InputReader(inputStream);
@@ -23,15 +26,22 @@ public class FindingVince {
         }
     }
     
-    static int minDistance(long[] dist, Boolean[] sptSet) {
-        double min = Double.MAX_VALUE;
-        int min_index = -1;
-        for (int v = 0; v < n; v++)
-            if (!sptSet[v] && dist[v] <= min) {
-                min = dist[v];
-                min_index = v;
-            }
-        return min_index;
+    static int minDistance(Boolean[] sptSet) {
+//        double min = Double.MAX_VALUE;
+//        int min_index = -1;
+//        for (int v = 0; v < n; v++)
+//            if (!sptSet[v] && dist[v] <= min) {
+//                min = dist[v];
+//                min_index = v;
+//            }
+//        return min_index;
+        int k;
+        do {
+            assert queue.peek() != null;
+            k = queue.peek().getKey();
+            queue.remove();
+        } while (sptSet[k]);
+        return k;
     }
     
     static long printSolution() {
@@ -47,23 +57,23 @@ public class FindingVince {
 //            }
 //        }
 //        return result;
-        return dist[n - 1];
+        return dist.get(n - 1);
     }
     
     static void dijkstra(int src) {
         graph[src].time = 0;
-        dist = new long[n];
+        dist = new HashMap<>();
         Boolean[] sptSet = new Boolean[n];
         // Initialize all distances as INFINITE and stpSet[] as false
         for (int i = 0; i < n; i++) {
-            dist[i] = Long.MAX_VALUE;
+            dist.put(i, Long.MAX_VALUE);
             sptSet[i] = false;
         }
         int diff, actual;
-        dist[src] = 0;
-        // Find shortest path for all vertices
+        dist.replace(src, (long) 0);
         for (int cnt = 0; cnt < n - 1; cnt++) {
-            int u = minDistance(dist, sptSet);
+            queue.addAll(dist.entrySet());
+            int u = minDistance(sptSet);
             sptSet[u] = true;
             vertex v = graph[u];
             
@@ -73,11 +83,12 @@ public class FindingVince {
                 if (diff < graph[k].a) {
                     actual += graph[k].a - diff;
                 }
-                if (!sptSet[k] && dist[u] != Double.MAX_VALUE && dist[u] + actual < dist[k]) {
-                    dist[k] = dist[u] + actual;
+                if (!sptSet[k] && dist.get(u) != Double.MAX_VALUE && dist.get(u) + actual < dist.get(k)) {
+                    dist.replace(k, dist.get(u) + actual);
                     graph[k].pre = u;
                 }
             }
+            queue.clear();
         }
     }
     
