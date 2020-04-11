db1869e62cc974cde4fbbe11f280460ecdbfdbf2
Warpeas <M4A1go@163.com>
2020-04-12 02:16:48 +0800

    all array version, the same

--- src/TheGreatMystery.java
+++ src/TheGreatMystery.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+
 import java.io.*;
 import java.util.*;
 
@@ -7,27 +9,6 @@ public class TheGreatMystery {
     static InputReader in = new InputReader(inputStream);
     static PrintWriter out = new PrintWriter(outputStream);
     static int n, m;
-    
-    static class node {
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            node node = (node) o;
-            return x == node.x &&
-                    y == node.y;
-        }
-        
-        int x, y;
-        int w, s;
-        
-        node(int x, int y, int w) {
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            s = -1;
-        }
-    }
     
     static class path implements Comparable<path> {
         int x1, y1;
@@ -48,53 +29,54 @@ public class TheGreatMystery {
         }
     }
     
-    static node[][] nodes;
-    static Map<Integer, Set<node>> setMap = new HashMap<>();
+    static int[][] weight;
+    static int[][] s;
+    static Map<Integer, Set<Integer>> setMap = new HashMap<>();
     static path[] paths;
     
     static long addToSet() {
         long result = 0;
-        int s1, s2, cnt = 0;
+        int s1, s2, cnt = 1;
         int x1, x2, y1, y2;
         for (int i = 0; i < paths.length && setMap.entrySet().size() != n * m; i++) {
             x1 = paths[i].x1;
             y1 = paths[i].y1;
             x2 = paths[i].x2;
             y2 = paths[i].y2;
-            s1 = nodes[x1][y1].s;
-            s2 = nodes[x2][y2].s;
-            if (s1 != s2 && s1 != -1 && s2 != -1) {
+            s1 = s[x1][y1];
+            s2 = s[x2][y2];
+            if (s1 != s2 && s1 != 0 && s2 != 0) {
                 result += paths[i].w;
                 if (s1 > s2) {
-                    for (node n :
+                    for (int p :
                             setMap.get(s1)) {
-                        n.s = s2;
+                        s[p / m][p % m] = s2;
                     }
                     setMap.get(s2).addAll(setMap.get(s1));
                     setMap.remove(s1);
                 } else {
-                    for (node n :
+                    for (int p :
                             setMap.get(s2)) {
-                        n.s = s1;
+                        s[p / m][p % m] = s1;
                     }
                     setMap.get(s1).addAll(setMap.get(s2));
                     setMap.remove(s2);
                 }
-            } else if (s1 == -1 && s2 != -1) {
+            } else if (s1 == 0 && s2 != 0) {
                 result += paths[i].w;
-                setMap.get(s2).add(nodes[x1][y1]);
-                nodes[x1][y1].s = s2;
-            } else if (s1 != -1 && s2 == -1) {
+                setMap.get(s2).add(x1 * m + y1);
+                s[x1][y1] = s2;
+            } else if (s1 != 0 && s2 == 0) {
                 result += paths[i].w;
-                setMap.get(s1).add(nodes[x2][y2]);
-                nodes[x2][y2].s = s1;
-            } else if (s1 == -1) {
+                setMap.get(s1).add(x2 * m + y2);
+                s[x2][y2] = s1;
+            } else if (s1 == 0) {
                 result += paths[i].w;
                 setMap.put(cnt, new HashSet<>());
-                setMap.get(cnt).add(nodes[x1][y1]);
-                setMap.get(cnt).add(nodes[x2][y2]);
-                nodes[x1][y1].s = cnt;
-                nodes[x2][y2].s = cnt++;
+                setMap.get(cnt).add(x1 * m + y1);
+                setMap.get(cnt).add(x2 * m + y2);
+                s[x1][y1] = cnt;
+                s[x2][y2] = cnt++;
             }
         }
         return result;
@@ -104,26 +86,26 @@ public class TheGreatMystery {
         long start = System.currentTimeMillis();
         n = in.nextInt();
         m = in.nextInt();
-        nodes = new node[n][m];
+        weight = new int[n][m];
+        s = new int[n][m];
         int w;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 w = in.nextInt();
-                nodes[i][j] = new node(i, j, w);
+                weight[i][j] = w;
             }
         }
-        int weight;
         paths = new path[n * (m - 1) + m * (n - 1)];
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (i + 1 < n) {
-                    weight = nodes[i][j].w ^ nodes[i + 1][j].w;
-                    paths[cnt++] = new path(i, j, i + 1, j, weight);
+                    w = weight[i][j] ^ weight[i + 1][j];
+                    paths[cnt++] = new path(i, j, i + 1, j, w);
                 }
                 if (j + 1 < m) {
-                    weight = nodes[i][j].w ^ nodes[i][j + 1].w;
-                    paths[cnt++] = new path(i, j, i, j + 1, weight);
+                    w = weight[i][j] ^ weight[i][j + 1];
+                    paths[cnt++] = new path(i, j, i, j + 1, w);
                 }
             }
         }
