6e93dcdc6ef73d961d091f2c82f48d7acd3b8f86
Warpeas <M4A1go@163.com>
2020-04-20 21:00:44 +0800

    n

--- src/MalaysianTranslator.java
+++ src/MalaysianTranslator.java
@@ -2,64 +2,50 @@
 import java.io.*;
 import java.util.*;
 
-import static java.util.Arrays.sort;
-
 public class MalaysianTranslator {
-//    static int Huffuman(Integer[] a, int n) {
-//        int sum;
-//        sort(a);
-//        if (n > 1) {
-//            sum = a[0] + a[1];
-//            a[1] = sum;
-//            Integer[] b = new Integer[n - 1];
-//            for (int i = 0; i < n - 1; i++) {
-//                b[i] = a[i + 1];
-//            }
-//            return sum + Huffuman(b, n - 1);
-//        }
-//        return 0;
-//    }
-    
-    static class c {
-        static int index = 0;
-        char c;
-        int cnt;
-    }
-    
-    static Map<Character, Integer> map;
-    static PriorityQueue<Integer> priorityQueue;
+    static Map<Character, Long> map;
+    static PriorityQueue<Long> priorityQueue;
     
     public static void main(String[] args) {
+        long start = System.currentTimeMillis();
         InputStream inputStream = System.in;
         OutputStream outputStream = System.out;
         InputReader in = new InputReader(inputStream);
         PrintWriter out = new PrintWriter(outputStream);
         int n = in.nextInt();
         char[] chars;
+        map = new HashMap<>();
+        priorityQueue = new PriorityQueue<>();
         for (int j = 0; j < n; j++) {
-            map = new HashMap<>();
-            priorityQueue = new PriorityQueue<>();
+            map.clear();
+            priorityQueue.clear();
             chars = in.nextCharArray();
             for (char c : chars) {
                 if (!map.containsKey(c)) {
-                    map.put(c, 1);
+                    map.put(c, (long) 1);
                 } else {
                     map.put(c, map.get(c) + 1);
                 }
             }
             priorityQueue.addAll(map.values());
-            int result = 0;
-            int a, b, temp;
-            while (priorityQueue.size() > 1) {
-                a = priorityQueue.poll();
-                b = priorityQueue.poll();
-                temp = a + b;
-                result += temp;
-                priorityQueue.add(temp);
+            long result = 0;
+            long a, b, temp;
+            if (priorityQueue.size() == 1) {
+                result = priorityQueue.poll();
+            } else {
+                while (priorityQueue.size() > 1) {
+                    a = priorityQueue.poll();
+                    b = priorityQueue.poll();
+                    temp = a + b;
+                    result += temp;
+                    priorityQueue.add(temp);
+                }
             }
             out.println(result);
-//            out.println(Huffuman(map.values().toArray(new Integer[0]), map.values().size()));
         }
+        long end = System.currentTimeMillis();
+        out.println();
+        out.println(end - start);
         out.close();
     }
     
--- src/generator.java
+++ src/generator.java
@@ -1,15 +1,27 @@
+import java.util.Random;
 
 public class generator {
+    public static String getRandomString(int length) {
+        String str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(62);
+            sb.append(str.charAt(number));
+        }
+        return sb.toString();
+    }
+    
     public static void main(String[] args) {
 //        int n = (int)(Math.random() * 5);
-        int n = (int) (2000 * Math.random());
-        n=200;
+        int n = (int) (10 * Math.random());
+        n = 1;
         System.out.println(n);
+        int s;
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                System.out.printf("%d ", (int) (Math.random() * 1000000000));
-            }
-            System.out.println();
+            s = (int) (Math.random() * 10000);
+            s = 8;
+            System.out.println(getRandomString(s));
         }
     }
 }
